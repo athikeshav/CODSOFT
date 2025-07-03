aab72278462cba6bafe2fb3deaf1d72345f7679e
Athi <42793268+athikeshav@users.noreply.github.com>
2025-07-03 14:44:53 +0530

    Update and rename Password Generator to codsoft_3/password generator.py

---
+++ codsoft_3/password generator.py
@@ -0,0 +1,11 @@
+#Password Generator
+import random
+import string
+print("Password Generator")
+length=int(input("enter the desired length of the password:"))
+i=0
+password=''
+for i in range(length):
+    password+=random.choice(string.ascii_letters)
+    
+print("password = ",password)
